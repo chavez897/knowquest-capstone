58b4a62ab3b9e1f2021639cb6e609991f411f44f
Bo Liu <lb1996914@gmail.com>
2022-05-15 22:54:55 -0400

    add and modify index page components

---
+++ front/src/components/ui/CardData.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id: 1,
+        title: "Rate your textbook",
+        description: "Use your Textbook’s ISBN number to Anonymously Rate Your Textbook! Your ratings will help improve textbooks.",
+    },
+    {
+        id: 2,
+        title: "Rate your school",
+        description: "Rate your school every semester! It takes less than a minute and helps your school improve its value to students.",
+    },
+    {
+        id: 3,
+        title: "Rate your class",
+        description: "Rate each class, every semester. Your ratings will help your school and professors to improve classes. Make your voice heard!",
+    },
+    {
+        id: 4,
+        title: "Rate your resource",
+        description: "Rate each class, every semester. Your ratings will help your school and professors to improve classes. Make your voice heard!",
+    },
+    {
+        id: 5,
+        title: "Rate your online learning",
+        description: "Online learning is the new normal in many places. Make sure your school knows how they are doing!",
+    }
+]
--- front/src/components/ui/NavBar.js
+++ front/src/components/ui/NavBar.js
@@ -9,31 +9,100 @@ export const NavBar = () => {
     dispatch(logout());
   };
   return (
-    <nav className="navbar navbar-expand-sm navbar-light bg-light px-3">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
       <div className="container">
-        <div className="col-lg-6"><img src="%PUBLIC_URL%/favicon.ico"></img></div>
-        {/* <div className="col">
-          <Link to="/index">
-            <p className="navbar-brand text-white">KnowQuest</p>
-          </Link>
-        </div> */}
-        <div className="col-lg-1"><p className="navbar-brand">Home</p></div>
-        <div className="col dropdown">
-          <p className="navbar-brand">Rate</p>
-        </div>
-        <div className="col-lg-1"><p className="navbar-brand">Search</p></div>
-        <div className="col-lg-1"><p className="navbar-brand">Partners</p></div>
-        <div className="col-lg-1"><p className="navbar-brand">Contests</p></div>
-        <div className="col-lg-1">
-          <p
-            className="btn nav-link text-decoration-none"
-            onClick={handleLogout}
-          >
-            Logout
-          </p>
+        <a className="navbar-brand " href="#">
+          <b style={{ color: "#333999" }}>
+            Know<span style={{ color: "#f93" }}>Quest</span>
+          </b>
+        </a>
+      </div>
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Rate
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Textbook
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    School
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Class
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Online Learning
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Resource
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Wekipedia
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Search
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Partners
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contests
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link btn" onClick={handleLogout}>
+                Logout
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-      
     </nav>
   );
 };
