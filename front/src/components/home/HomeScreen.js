2e6dc0998303465adb95f52e918b59f189291cd8
Chavez <rodrichavezm@gmail.com>
2022-05-16 14:24:29 -0400

    card data

--- front/src/components/home/HomeScreen.js
+++ front/src/components/home/HomeScreen.js
@@ -5,26 +5,28 @@ import { Announcenment } from "../ui/Announcement";
 import { IndexSignUp } from "../ui/IndexSignUp";
 
 export const HomeScreen = () => {
+  const title = "test"
+  const description = "easdf"
   return (
     <div className="container-fluid">
       <IndexSignUp />
       <div className="container">
         <div className="row">
           <div className="col-lg-4">
-            <Card />
+            <Card title={title} description={description}/>
+          </div>
+          {/* <div className="col-lg-4">
+            <Card title={title} description={description}/>
           </div>
           <div className="col-lg-4">
-            <Card />
+            <Card key="1" title={title} description={description}/>
           </div>
           <div className="col-lg-4">
-            <Card />
+            <Card title={title} description={description}/>
           </div>
           <div className="col-lg-4">
-            <Card />
-          </div>
-          <div className="col-lg-4">
-            <Card />
-          </div>
+            <Card title={title} description={description}/>
+          </div> */}
         </div>
       </div>
       <Announcenment />
