a1c1af7d533976ad45c6451a522e025e69214af8
Bo Liu <lb1996914@gmail.com>
2022-06-05 20:33:47 -0400

    add rate book frontend

--- front/src/routers/UserRouter.js
+++ front/src/routers/UserRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { UserProfileScreen } from "../components/user/UserProfileScreen";
 import { ReferralScreen } from "../components/referral/ReferralScreen";
+import { RateBookScreen } from "../components/rate/RateBookScreen";
 
 export const UserRouter = () => {
   return (
@@ -10,6 +11,7 @@ export const UserRouter = () => {
         <Switch>
           <Route exact path="/user/profile" component={UserProfileScreen} />
           <Route exact path="/user/referral" component={ReferralScreen} />
+          <Route exact path="/user/ratebook" component={RateBookScreen} />
 
           <Redirect to="/home" />
         </Switch>
