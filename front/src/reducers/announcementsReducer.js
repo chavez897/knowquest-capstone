cdfca16f232d4e35deb0305407c566501c62a9b4
Chavez <rodrichavezm@gmail.com>
2022-06-30 12:04:22 -0400

    fix

--- front/src/reducers/announcementsReducer.js
+++ front/src/reducers/announcementsReducer.js
@@ -3,7 +3,7 @@ import { types } from "../types/types";
 export const announcementsReducer = (state = [], action) => {
   switch (action.type) {
     case types.saveAnnouncements:
-      return action.payload.announcements;
+      return action.payload.annoucements;
     case types.deleteAnnouncements:
       return [];
     default:
