646ef22469162d1858170ef5357f8a4be8e6a9b2
elwincheng <elwinton3.14@gmail.com>
2022-07-19 14:47:02 -0700

    Added download image function

--- front/src/components/contests/ContestCard.js
+++ front/src/components/contests/ContestCard.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import './ContestCard.css'
 
 export const ContestCard = ({src}) => {
+  
+  const download = () => {
+    var element = document.createElement("a");
+    var file = new Blob(
+      [
+        src
+      ],
+      { type: "image/*" }
+    );
+    element.href = URL.createObjectURL(file);
+    element.download = "ContestImage.jpg";
+    element.click();
+  };
+
 	return (
 		<div className="card hover-shadow bg-light text-black">
-			<img 
-				className="card-img-top" 
-				src={src} 
-				alt=""
-			/>
+      <a 
+        className="download-link" 
+        title="Contest Image" 
+        onClick={() => download()}
+      >
+        <img 
+          className="card-img-top" 
+          src={src} 
+          alt="Contest Image"
+        />
+        <div class="card-footer download-btn">
+            <i class="fa fa-download"></i> 
+            Download Image
+        </div>
+      </a>
 		</div>
 	);
 };
